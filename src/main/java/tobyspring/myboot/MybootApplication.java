4262ac13bec10dafe9a566f7c7807de8eeee4776
happysubin <asb0711@gmail.com>
2023-01-19 20:57:28 +0900

    Feat: 서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -1,8 +1,18 @@
 package tobyspring.myboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 public class MybootApplication {
 
 	public static void main(String[] args) {
 
+		/**
+		 * 톰캣 웹 서버, 즉 웹 컨테이너인 서블릿 컨테이를 실행하는 코드.
+		 * TomcatServletWebServerFactory를 사용해 복잡한 설정을 생략 가능.
+		 */
+		TomcatServletWebServerFactory servletFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = servletFactory.getWebServer();
+		webServer.start();
 	}
 }
