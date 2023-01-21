e9d6d47a90ffbac440d8d885ec36b93a7a425ac8
happysubin <asb0711@gmail.com>
2023-01-21 12:37:56 +0900

    Feat: 디스패처 서블릿과 애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -2,22 +2,11 @@ package tobyspring.myboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import tobyspring.myboot.hello.HelloController;
 import tobyspring.myboot.hello.SimpleHelloService;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MybootApplication {
 
@@ -30,42 +19,20 @@ public class MybootApplication {
 		TomcatServletWebServerFactory servletFactory = new TomcatServletWebServerFactory();
 
 
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class); //인터페이스가 아닌 구체 클래스를 넣어줘야함.
 		applicationContext.refresh(); //구성 정보를 가지고 컨테이너를 초기화함. 빈 오브젝트를 다 만듦.
+
 		/**
 		 * 디스패처 서블릿을 등록
 		 * 웹 앱 로직은 다른 객체에게 책임을 위임해야함.
 		 */
 
-		WebServer webServer = servletFactory.getWebServer(new ServletContextInitializer() {
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				servletContext.addServlet("frontcontroller", new HttpServlet() {
-					@Override
-					public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-
-							HelloController controller = applicationContext.getBean(HelloController.class);
-							String name = controller.hello(req.getParameter("name"));
-
-							//resp.setStatus(HttpStatus.OK.value()); 서블릿이 알아서 200번이나 오류가 발생하면 적절한 상태 코드를 리턴함.
-
-							resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-							resp.getWriter().println(name);
-						}
-
-						else if (req.getRequestURI().equals("/user")){
-							//
-						}
-						else{
-							resp.setStatus(HttpStatus.NOT_FOUND.value());
-						}
-					}
-				}).addMapping("/*");
-			}
+		WebServer webServer = servletFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+			).addMapping("/*");
 		});
 		webServer.start();
 	}
--- src/main/java/tobyspring/myboot/hello/HelloController.java
+++ src/main/java/tobyspring/myboot/hello/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.myboot.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +16,7 @@ public class HelloController {
         this.helloService = simpleHelloService;
     }
 
+    @GetMapping
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
