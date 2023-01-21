729295f758cf46ac36e82279602df2fbf1ad4094
happysubin <asb0711@gmail.com>
2023-01-21 13:21:45 +0900

    Feat: 자바 코드 구성 정보 사용

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -4,19 +4,34 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import tobyspring.myboot.hello.HelloController;
+import tobyspring.myboot.hello.HelloService;
 import tobyspring.myboot.hello.SimpleHelloService;
 
+@Configuration
 public class MybootApplication {
+
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService); }
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 
 		/**
 		 * 서블릿 컨테이너 작업 및 초기화를 스프링 컨테이너 초기화 과정중에 발생하도록 수정. 스프링 부트가 이런 방식.
+		 * AnnotationConfigWebApplicationContext 애노테이션 자바 구성을 인식할 수 있음.
 		 */
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -29,8 +44,7 @@ public class MybootApplication {
 			}
 		};
 
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class); //인터페이스가 아닌 구체 클래스를 넣어줘야함.
+		applicationContext.register(MybootApplication.class);
 		applicationContext.refresh(); //구성 정보를 가지고 컨테이너를 초기화함. 빈 오브젝트를 다 만듦. 템플릿 메서드 방식임.
 	}
 }
