4846947cd2f1739de16ee712564044525cca6b4d
happysubin <asb0711@gmail.com>
2023-01-21 13:54:59 +0900

    Feat: SpringApplication을 적용하도록 변경

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -1,31 +1,31 @@
-package tobyspring.myboot;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
-
-@Configuration
-@ComponentScan
-public class MybootApplication {
-
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
-
-	/**
-	 * 스프링 부트 애플리케이션과 똑같은 모습이 되었다. 대박.
-	 */
-	public static void main(String[] args) {
-		MySpringApplication.run(MybootApplication.class, args);
-	}
-}
+//package tobyspring.myboot;
+//
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//
+//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.DispatcherServlet;
+//
+//@Configuration
+//@ComponentScan
+//public class MybootApplication {
+//
+//	@Bean
+//	public ServletWebServerFactory servletWebServerFactory() {
+//		return new TomcatServletWebServerFactory();
+//	}
+//
+//	@Bean
+//	public DispatcherServlet dispatcherServlet() {
+//		return new DispatcherServlet();
+//	}
+//
+//	/**
+//	 * 스프링 부트 애플리케이션과 똑같은 모습이 되었다. 대박.
+//	 */
+//	public static void main(String[] args) {
+//		MySpringApplication.run(MybootApplication.class, args);
+//	}
+//}
