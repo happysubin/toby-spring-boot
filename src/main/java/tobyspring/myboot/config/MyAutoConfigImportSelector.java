af1a12e01288f853c1aa056323445cc00c3a2a39
happysubin <asb0711@gmail.com>
2023-01-22 14:24:37 +0900

    Feat: 자동 구성 정보 파일 분리

--- src/main/java/tobyspring/myboot/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/myboot/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,29 @@
 package tobyspring.myboot.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//BeanClassLoaderAware 인터페이스를 구현하면 세터에서 꽂아줌.
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.myboot.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.myboot.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+        // META-INF/spirng 아래에 imports가 붙은 파일에서 컨테이너에 등록시킬 클래스 목록을 가져와서 등록한다.
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        //return autoConfigs.toArray(new String[0]); 이렇게도 가능.
+        return autoConfigs.toArray(String[]::new);
     }
 }
