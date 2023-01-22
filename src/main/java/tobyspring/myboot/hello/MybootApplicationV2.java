92490e2fa054fdd0fad6b8573766b623dd1d634e
happysubin <asb0711@gmail.com>
2023-01-22 13:49:05 +0900

    Feat: 인프라 빈 구성 정보의 분리
    AutoConfiguration을 도입하는 첫 과정

--- src/main/java/tobyspring/myboot/hello/MybootApplicationV2.java
+++ src/main/java/tobyspring/myboot/hello/MybootApplicationV2.java
@@ -2,9 +2,9 @@ package tobyspring.myboot.hello;
 
 import org.springframework.boot.SpringApplication;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tobyspring.myboot.config.MySpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 @MySpringBootApplication
 public class MybootApplicationV2 {
 
