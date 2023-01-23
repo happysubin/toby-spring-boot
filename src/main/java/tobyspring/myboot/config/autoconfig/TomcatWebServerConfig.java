4d5dd6756b79259a2f07465640c0d24351df0a03
happysubin <asb0711@gmail.com>
2023-01-23 16:26:55 +0900

    Feat: 프로퍼티 클래스의 분리

--- src/main/java/tobyspring/myboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/myboot/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.myboot.config.autoconfig;
 
+import org.apache.catalina.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -14,14 +15,12 @@ import tobyspring.myboot.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Value("${contextPath}")
-    String contextPath;
-
     @Bean("tomcatWebserverFactory")
     @ConditionalOnMissingBean //똑같은 타입의 빈이 없어야지 빈으로 등록된다.
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties serverProperties) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(this.contextPath);
+        serverFactory.setContextPath(serverProperties.getContextPath());
+        serverFactory.setPort(serverProperties.getPort());
         return serverFactory;
     }
 }
